b862fbc7ca90065d9242b9442a927293cb1faa98
Blaine Dannheisser <blainecd@gmail.com>
2021-01-25 20:24:53 -0500

    Project 1 in progress

---
+++ Project1_StormViewer/Project1/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Blaine Dannheisser on 1/19/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+                debugPrint(pictures)
+            }
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let viewController = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            viewController.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+
+}
+
